4c583ab3e31d9d1e3b41e6f5c439af9384bb6d49
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 00:24:11 +0700

    DungNguyen

---
+++ BaiTap3/Diem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3
+{
+    internal class Diem
+    {
+        double x, y;
+        public Diem()
+        {
+            x = 0; y = 0;
+        }
+        public Diem(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void nhap(string s)
+        {
+            try
+            {
+                Console.Write("Nhap toa do {0}.x : ",s);
+                x = double.Parse(Console.ReadLine());
+                Console.Write("Nhap toa do {0}.y : ", s);
+                y = double.Parse(Console.ReadLine());
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void xuat(string s)
+        {
+            Console.Write("\nToa do diem {0}({1},{2})",s,x.ToString(),y.ToString());
+        }
+
+        public double khoangCach(Diem B)
+        {
+            return Math.Sqrt((x-B.x)*(x-B.x)+(y-B.y)*(y-B.y));
+        }
+    }
+}
---
+++ BaiTap3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diem A = new Diem();
+            Diem B = new Diem();
+
+            A.nhap("A");
+            B.nhap("B");
+
+            A.xuat("A");
+            B.xuat("B");
+
+            Console.WriteLine("\nAB = " + A.khoangCach(B));   // khoang cach
+        }
+    }
+}
